47615a5a14780e67ee5b01fe292b69e1e24dd190
PRANAV TRIPATHI <76243022+PranavTrip@users.noreply.github.com>
2020-12-22 11:29:48 +0530

    PRINTING PATTERN 2

---
+++ loops13.c
@@ -0,0 +1,22 @@
+//PRINTING PATTERNS
+/* 
+***********
+***********
+***********
+***********
+*********** 
+*/
+
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\n");
+        for (int j = 0; j <= 10; j++)
+        {
+            printf("*");
+        }
+    }
+    return 0;
+}
